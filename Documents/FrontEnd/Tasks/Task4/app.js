b8941651ebf43b9f47252d26e2dc9143bdd9ae51
Pakizər Məsimli <pakizerrmesimli@gmail.com>
2021-06-27 10:47:31 +0400

    js tasks update

---
+++ Documents/FrontEnd/Tasks/Task4/app.js
@@ -0,0 +1,27 @@
+students=[
+    {
+        ad:'Samir',
+        soyad:'Kerimov',
+        yas:40
+    },
+    {
+        ad:'Hesen',
+        soyad:'Kerimov',
+        yas:35
+    },
+    {
+        ad:'Hesen',
+        soyad:'Serdarov',
+        yas:23
+    },
+    {
+        ad:'Malik',
+        soyad:'Quliyev',
+        yas:23
+    }
+]
+
+// adi hesen olan istifadecileri goster
+// soyadi Kerimov olan istifadecileri goster
+// adlari eyni olan istifadecileri goster
+// soyadlari eyni olan istifadecileri goster
