4720212a5fbf1ab1980237034c75d84d773faa4e
Pakizer <pakizerrmesimli@gmail.com>
2021-11-23 20:25:06 +0400

    signup and login pages update

--- FrontEnd/React/InstagramCloneApp/instagram/src/pages/login.js
+++ FrontEnd/React/InstagramCloneApp/instagram/src/pages/login.js
@@ -34,7 +34,7 @@ export default function Login() {
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
       <div className="flex w-3/5">
         <img
-          src="/images/iphone-with-profile.jpg"
+          src="/images/iphone-with-profile.jpeg"
           alt="iPhone with Instagram app"
         />
       </div>
@@ -42,7 +42,7 @@ export default function Login() {
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
           <h1 className="flex justify-center w-full">
             <img
-              src="/images/logo.png"
+              src="/images/logo .png"
               alt="Instagram"
               className="mt-2 w-6/12 mb-4"
             />
--- FrontEnd/React/InstagramCloneApp/instagram/src/pages/sign-up.js
+++ FrontEnd/React/InstagramCloneApp/instagram/src/pages/sign-up.js
@@ -67,7 +67,7 @@ export default function SignUp() {
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
       <div className="flex w-3/5">
         <img
-          src="/images/iphone-with-profile.jpg"
+          src="/images/iphone-with-profile.jpeg"
           alt="iPhone with Instagram app"
         />
       </div>
@@ -75,7 +75,7 @@ export default function SignUp() {
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
           <h1 className="flex justify-center w-full">
             <img
-              src="/images/logo.png"
+              src="/images/logo .png"
               alt="Instagram"
               className="mt-2 w-6/12 mb-4"
             />
